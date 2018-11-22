c27b56cd6361a598c15257cc351b5b589f2f1b0f
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-11-22 17:12:50 +0545

    Saves log to dropbox

--- garena-auto-spin.py
+++ garena-auto-spin.py
@@ -1,6 +1,9 @@
+import sys
+import os
 import requests
 import time
 import threading
+import dropbox
 
 
 url = 'http://luckydraw2.gamehub.sg.garenanow.com/service/luckydraw/'
@@ -11,14 +14,25 @@ data = {'sk': 'e7bd0ab670db68f6210adcf22c6c3663fd14213854252104453c41e55962bd34'
         'version': 1542111453
         }
 
+dbx = dropbox.Dropbox(
+    'ngaX7suUUyAAAAAAAAAAKT2rkFTWb7-R-pDsJ9bcin5Myi-1pbTfDcdM4pSg2qYA')
+
+
+def upload_to_dropbox(filename, data):
+    print ("Attempting to upload...")
+    try:
+        dbx.files_upload(data, '/' + filename, mute=True)
+    except Exception as err:
+        print("Failed to upload")
+        print(err)
+    print("Finished up load.")
+
 
 def send():
     response = requests.post(url, data=data)
     print(response.content)
-    threading.Timer(28800, send).start()
-    file = open(str(time.ctime())+".json", "w")
-    file.write(response.content.decode("utf-8"))
-    file.close()
+    upload_to_dropbox(str(time.ctime()) + ".json", response.content)
+    threading.Timer(5, send).start()
 
 
 send()
